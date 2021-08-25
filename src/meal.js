db70e0910c1ddfaaf3005b8441ba47feabd4817c
Brian Peterson <bpeterson1579@gmail.com>
2021-08-25 16:23:07 -0600

    Finish meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`;
 }
 
+function createMenuItem(foodName, foodPrice, mealType) {
+  var menuItem = {
+    name: foodName,
+    price: foodPrice,
+    type: mealType,
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, nameOfArray) {
+  if (nameOfArray.includes(ingredient)) {
+    return
+  }else {
+    nameOfArray.push(ingredient);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, mealType) {
+  return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: mealType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
